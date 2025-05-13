552401c7d26be44fe96a13ca3a707ec39aa0a2fd
chanti7d <leelakrishnaponnathi@gmail.com>
2025-05-13 16:25:18 +0530

    init

---
+++ index.js
@@ -0,0 +1,34 @@
+// let x=90;
+// let y=20;
+// console.log(x+y);
+
+
+// let firstname = "chanti"
+
+// let age = 24;
+
+// let userExists = true
+
+// console.log(typeof userExists)
+// console.log(typeof firstname)
+
+
+// console.log("My name is " + firstname, "and my age is " , age)
+
+// console.log(`My last name is ${firstname} and my age is ${age}`)
+
+// console.log(5 === "5")
+// console.log(1 === true)
+// console.log(null === undefined);
+
+
+
+//=====================Ternary operator ===========//
+
+
+
+
+
+
+
+
